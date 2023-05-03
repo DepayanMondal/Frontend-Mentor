7d2d2fe926e575edcb538fa9f64cc42af3d4a38d
Depayan M <72678445+DepayanMondal@users.noreply.github.com>
2023-05-03 19:18:40 +0530

    Delete Java directory

--- Java/JavaBasics.java
+++
@@ -1,8 +0,0 @@
-public class JavaBasics {
-    public static void main(String[] args) {
-        System.out.print("Hello World!\n");
-        System.out.println("Hello 2nd line");
-        System.out.println("1 2 3 4 5");
-    }
-}
-/* boilerplate code */
--- Java/homeworks/Rainwater.java
+++
@@ -1,5 +0,0 @@
-public class Rainwater {
-    public static void main(String[] args) {
-
-    }
-}
